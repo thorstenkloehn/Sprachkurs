f8f43ea6ece00930ff0aa40910966df37669e108
Thorsten Klöhn <thorstenkloehn@gmail.com>
2021-08-09 00:37:45 +0200

    Test

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/thorstenkloehn/Sprachkurs
+
+go 1.16
+
+require (
+	github.com/mattn/go-sqlite3 v1.14.8 // indirect
+	gorm.io/driver/sqlite v1.1.4 // indirect
+	gorm.io/gorm v1.21.12 // indirect
+)
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"os"
+	"time"
+)
+
+type Vokabelm struct {
+	Id              int       `json:"id"`
+	VokabelEnglisch string    `json:"Vokabel_Englisch"`
+	VokabelDeutsch  string    `json:"Vokabel_Deutsch"`
+	Lauschrift      string    `json:"Lauschrift"`
+	Datum           time.Time `json:"Datum"`
+}
+
+func main() {
+
+	if _, err := os.Stat("vokabeln.db"); os.IsNotExist(err) {
+		fmt.Println("file does not exist")
+
+		db, err := gorm.Open(sqlite.Open("vokabeln.db"), &gorm.Config{})
+		if err != nil {
+			panic("Fehler bei der Verbindung zur Datenbank")
+		}
+		db.AutoMigrate(&Vokabelm{})
+	}
+
+}
